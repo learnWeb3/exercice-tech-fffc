b70e771c6aeec4e4c88cb43f323473cd4efcaa89
Ewie <cha.gautron@gmail.com>
2023-07-24 11:30:51 +0200

    feat(index.js/home.ejs):Create a function to download the files after it was converted

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const fs = require("fs");
+const { zip } = require("zip-a-folder");
 const uploadMiddleware = require("./middlewares/uploadMiddleware"); // Importer le middleware
 const { readAllFiles } = require("./DataMapper/readAllFiles"); // Importer la fonction readAllFiles
 
@@ -28,6 +29,42 @@ app.post("/upload", uploadMiddleware, async (req, res) => {
     res.json(errors); // Renvoyer les erreurs sous forme de tableau JSON
   } catch (error) {
     res.sendStatus(500); // Répondre avec un statut d'erreur 500
+  }
+});
+app.get("/download", async (req, res) => {
+  const outputFolderPath = path.join(__dirname, "public", "OutputData");
+  const tempFolderPath = path.join(__dirname, "temp"); // Dossier temporaire pour stocker l'archive ZIP
+  const outputFilePath = path.join(tempFolderPath, "fichiers_convertis.zip");
+
+  try {
+    // Vérifier si le dossier temporaire existe, sinon le créer
+    if (!fs.existsSync(tempFolderPath)) {
+      fs.mkdirSync(tempFolderPath);
+    }
+
+    // Créer l'archive ZIP du dossier 'OutputData' contenant les fichiers convertis
+    await zip(outputFolderPath, outputFilePath);
+
+    // Envoyer l'archive ZIP au client pour le téléchargement avec les en-têtes appropriés
+    res.download(outputFilePath, "fichiers_convertis.zip", (err) => {
+      if (err) {
+        console.error("Erreur lors du téléchargement du fichier :", err);
+        return res.sendStatus(500);
+      }
+
+      // Une fois le téléchargement terminé, supprimer le dossier temporaire et son contenu
+      fs.rmdir(tempFolderPath, { recursive: true }, (err) => {
+        if (err) {
+          console.error(
+            "Erreur lors de la suppression du dossier temporaire :",
+            err
+          );
+        }
+      });
+    });
+  } catch (error) {
+    console.error("Erreur lors de la création de l'archive ZIP :", error);
+    return res.sendStatus(500);
   }
 });
 
